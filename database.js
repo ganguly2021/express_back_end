ce67cf4fcacd84dbd2828ca3f20ac31622072a43
Git Job Work <jobaccount@abc.com>
2021-09-06 12:01:19 +0530

    index.js & database connection

---
+++ database.js
@@ -0,0 +1,20 @@
+// library import
+const mongoose = require('mongoose');
+const db_url = process.env.DB_URL;
+const assert = require('assert');
+
+// mongoose connect
+mongoose.connect(
+  db_url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (error, link) => {
+    // check error
+    assert.strictEqual(error, null, "Database connection failed.");
+
+    // Database connection established message
+    console.log("Database connection success.");
+  }
+)
